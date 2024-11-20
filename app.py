b4fb2fb9ca0803e6a734b1c52b9113ebc2e4c6f6
IsmaelMooBa <122620548+IsmaelMooBa@users.noreply.github.com>
2024-11-20 02:33:59 -0600

    Comunication between admin and users
    hola

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
-from flask import render_template, request, redirect, Response, url_for, session
-from flask_mysqldb import MySQL, MySQLdb  # pip install Flask-MySQLdb
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, redirect, Response, url_for, session
+from flask_mysqldb import MySQL, MySQLdb
 
-app = Flask(__name__, template_folder='template')
+
+app = Flask(__name__, template_folder='templates')
+
+app.config['UPLOAD_FOLDER'] = 'static/images'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+app.secret_key = 'secret_key'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -11,6 +17,8 @@ app.config['MYSQL_DB'] = 'login'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/listar-productos')
 def listar_productos():
@@ -28,9 +36,16 @@ def agregar_producto():
         nombre = request.form['nombre']
         precio = request.form['precio']
         cantidad = request.form['cantidad']
+        imagen = request.files['imagen']
+        if imagen and allowed_file(imagen.filename):
+            filename = secure_filename(imagen.filename)
+            imagen_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            imagen.save(imagen_path)
+        else:
+            filename = None  # En caso de que no se haya subido una imagen válida
         
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO productos (nombre, precio, cantidad) VALUES (%s, %s, %s)", (nombre, precio, cantidad))
+        cur.execute("INSERT INTO productos (nombre, precio, cantidad, imagen) VALUES (%s, %s, %s, %s)", (nombre, precio, cantidad, filename))
         mysql.connection.commit()
         cur.close()
         
@@ -51,8 +66,18 @@ def editar_producto(id):
         precio = request.form['precio']
         cantidad = request.form['cantidad']
         
-        cur.execute("UPDATE productos SET nombre = %s, precio = %s, cantidad = %s WHERE id = %s", 
-                    (nombre, precio, cantidad, id))
+        imagen = request.files['imagen']
+        if imagen and allowed_file(imagen.filename):
+            # Si hay una nueva imagen, la subimos y reemplazamos la anterior
+            filename = secure_filename(imagen.filename)
+            imagen_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            imagen.save(imagen_path)
+        else:
+            # Si no se sube una nueva imagen, mantenemos la actual
+            filename = producto['imagen']
+        
+        cur.execute("UPDATE productos SET nombre = %s, precio = %s, cantidad = %s, imagen = %s WHERE id = %s", 
+                    (nombre, precio, cantidad, filename, id))
         mysql.connection.commit()
         cur.close()
         
@@ -78,7 +103,16 @@ def home():
 
 @app.route('/admin')
 def admin():
-    return render_template('admin.html')   
+    return render_template('admin.html')  
+
+@app.route('/usuario')
+def usuario():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM productos")  # Consultar todos los productos
+    productos = cur.fetchall()
+    cur.close()
+    return render_template("usuario.html", productos=productos)
+ 
 
 @app.route('/acceso-login', methods=["GET", "POST"])
 def login():
@@ -89,18 +123,27 @@ def login():
         cur = mysql.connection.cursor()
         cur.execute('SELECT * FROM usuarios WHERE correo = %s AND password = %s', (_correo, _password,))
         account = cur.fetchone()
-
+        
         if account:
             session['logueado'] = True
             session['id'] = account['id']
             session['id_rol'] = account['id_rol']
 
-            if session['id_rol'] == 1:
-                return render_template("admin.html")
-            elif session['id_rol'] == 2:
-                return render_template("usuario.html")
+            if session['id_rol'] == 1:  # Administrador
+                # Consulta los productos para mostrarlos en la tabla
+                cur.execute("SELECT * FROM productos")
+                productos = cur.fetchall()
+                cur.close()
+                return render_template("admin.html", productos=productos)
+
+            elif session['id_rol'] == 2:  # Usuario estándar
+                cur.execute("SELECT * FROM productos")  # Obtener los productos
+                productos = cur.fetchall()
+                cur.close()
+                return render_template("usuario.html", productos=productos)
         else:
             return render_template('index.html', mensaje="Usuario o Contraseña Incorrectos")
+
 
 
 @app.route('/registro')
@@ -109,7 +152,8 @@ def registro():
 
 @app.route('/comprar')
 def comprar():
-    return render_template('comprar.html')  # Asegúrate de que "comprar.html" esté en la carpeta "templates".
+    return render_template('comprar.html')
+
 
 @app.route('/crear-registro', methods=["GET", "POST"])
 def crear_registro(): 
